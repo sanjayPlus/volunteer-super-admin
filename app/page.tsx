3e56f1ab735b1fd5a7225a9ed793e74d7d893acd
sanjayPlus <sanjaysanthoshplusitpark@gmail.com>
2024-02-28 17:10:46 +0530

    new

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
 "use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function Home() {
+  const router = useRouter();
+  useEffect(() => {
+    if(localStorage.getItem("token")){
+      router.push("/all-volunteer")
+    }else{
+      router.push("/login")
+    }
+  })
   return (
     <>
     
