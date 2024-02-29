e4b346a419c5d2c3788883d4a5f17c04230d62c3
sanjayPlus <sanjaysanthoshplusitpark@gmail.com>
2024-02-29 10:33:24 +0530

    hi

--- utils/SERVER_URL.js
+++ utils/SERVER_URL.js
@@ -1,4 +1,4 @@
-// const SERVER_URL = "http://localhost:5000/api";
-const SERVER_URL = "https://volunteer-4z7g.onrender.com/api";
+const SERVER_URL = "http://localhost:5000/api";
+// const SERVER_URL = "https://volunteer-4z7g.onrender.com/api";
 
 export default SERVER_URL;
