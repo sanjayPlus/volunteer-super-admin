2cf668de3e43edb3d5878605ebeb6f0406f53dd9
danaplusitpark <danaplusitpark@gmail.com>
2024-03-20 09:50:42 +0530

    whatsapp-public

---
+++ app/whatsapp-public/page.tsx
@@ -0,0 +1,380 @@
+"use client";
+import Sidebar from "@/components/Sidebar";
+import SERVER_URL from "@/utils/SERVER_URL";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+
+function AddWhatsappPublic() {
+  const [link, setLink] = useState("");
+  const [optional, setOptional] = useState("");
+  const [booth, setBooth] = useState("");
+  const [district, setDistrict] = useState("");
+  const [assembly, setAssembly] = useState("");
+  const [constituency, setConstituency] = useState("");
+  const [districtList, setDistrictList] = useState([]);
+  const [constituencyList, setConstituencyList] = useState([]);
+  const [assemblyList, setAssemblyList] = useState([]);
+  const [boothList, setBoothList] = useState([]);
+  const [whatsappPublic, setWhatsappPublic] = useState([]);
+
+  const router = useRouter();
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      router.push("/login");
+    }
+    axios
+      .get(`${SERVER_URL}/admin/protected`, {
+        headers: {
+          "x-access-token": token,
+        },
+      })
+      .then((res) => {})
+      .catch((err) => {
+        router.push("/login");
+        localStorage.removeItem("token");
+      });
+  }, []);
+  useEffect(() => {
+    axios.get((SERVER_URL + "/admin/whatsapp-public"),
+        {
+            headers: {
+                "x-access-token": localStorage.getItem("token")
+            }
+        }).then((res) => {
+            setWhatsappPublic(res.data);
+        });
+}, []);
+  useEffect(() => {
+    axios.get(SERVER_URL + "/admin/state-districtV1").then((res) => {
+      setDistrictList(res.data);
+    });
+  }, []);
+  const handleDistrictChange = (e: any) => {
+    const selectedDistrict = e.target.value; // Get the selected district from the event
+
+    setDistrict(selectedDistrict); // Update the district state with the selected district
+
+    axios
+      .get(
+        `${SERVER_URL}/admin/state-districtV1?district=${selectedDistrict}`,
+        {
+          // Use the updated district value
+          headers: { "x-access-token": localStorage.getItem("token") },
+        }
+      )
+      .then((userResponse) => {
+        if (userResponse.status === 200) {
+          setConstituencyList(userResponse.data);
+        }
+      })
+      .catch((err) => {
+        console.log(err.response.data);
+      });
+  };
+
+  const handleConstituencyChange = (e: any) => {
+    if (district == "") {
+      toast.error("Select The District");
+    }
+    const selectedConstituency = e.target.value; // Get the selected district from the event
+
+    setConstituency(selectedConstituency); // Update the district state with the selected district
+
+    axios
+      .get(
+        `${SERVER_URL}/admin/state-districtV1?district=${district}&constituency=${selectedConstituency}`,
+        {
+          // Use the updated district value
+          headers: { "x-access-token": localStorage.getItem("token") },
+        }
+      )
+      .then((userResponse) => {
+        if (userResponse.status === 200) {
+          setAssemblyList(userResponse.data);
+        }
+      })
+      .catch((err) => {
+        console.log(err.response.data);
+      });
+  };
+  const handleAssemblyChange = (e: any) => {
+    if (district == "") {
+      toast.error("Select The District");
+    }
+    if (constituency == "") {
+      toast.error("Select The Constituency");
+    }
+    const selectedAssembly = e.target.value; // Get the selected district from the event
+
+    setAssembly(selectedAssembly); // Update the district state with the selected district
+
+    axios
+      .get(
+        `${SERVER_URL}/admin/state-districtV1?district=${district}&constituency=${constituency}&assembly=${selectedAssembly}`,
+        {
+          // Use the updated district value
+          headers: { "x-access-token": localStorage.getItem("token") },
+        }
+      )
+      .then((userResponse) => {
+        if (userResponse.status === 200) {
+          setBoothList(userResponse.data);
+        }
+      })
+      .catch((err) => {
+        console.log(err.response.data);
+      });
+  };
+ 
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    const token = localStorage.getItem("token");
+    axios
+      .post(
+        `${SERVER_URL}/admin/add-whatsapp-public`,
+        {
+          link,
+          optional,
+          district,
+          assembly,
+          booth,   
+          constituency,
+          
+        },
+        {
+          headers: {
+            "x-access-token": token,
+          },
+        }
+      )
+      .then((res) => {
+        if (res.status === 200 || res.status === 201) {
+            setLink("");
+            setOptional("");
+            setDistrict("");
+            setAssembly("");
+            setBooth("");
+            setConstituency("");
+            toast.success("whatsapp public added successfully");
+            axios.get(`${SERVER_URL}/admin/whatsapp-public`).then((res) => {
+                if (res.status === 200) {
+                    setWhatsappPublic(res.data);
+                }
+            })
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  const handleDelete = (id: string) => {
+    axios
+        .delete(
+            `${SERVER_URL}/admin/whatsapp-public/${id}`,
+            {
+                headers: {
+                    "x-access-token": localStorage.getItem("token"),
+                },
+            }
+        )
+        .then((res) => {
+            if (res.status === 200) {
+
+                toast.success("whatsapp link deleted successfully");
+                setWhatsappPublic(whatsappPublic.filter((whatsapp: any) => whatsapp._id !== id));
+            }
+        });
+};
+  return (
+    <Sidebar>
+      <div>
+        <h1 className="text-3xl font-bold mx-auto">Add Whatsapp Public</h1>
+        
+        <div className="max-w-sm mx-auto mt-14 ">
+          <label
+            htmlFor="name"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Link
+          </label>
+          <input
+            onChange={(e) => setLink(e.target.value)}
+            type="text"
+            id="link"
+            value={link}
+            className="bg-gray-50 border mb-2 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="link"
+          />
+
+          {/* optional field */}
+          <label
+            htmlFor="optional"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Optional
+          </label>
+          <input
+            onChange={(e) => setOptional(e.target.value)}
+            type="text"
+            id="optional"
+            value={optional}
+            className="bg-gray-50 mb-2 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="optional"
+          />
+
+          
+
+          {/* district field */}
+        </div>
+        <div className="max-w-sm mx-auto">
+          <label
+            htmlFor="district"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Select District
+          </label>
+          <select
+            id="district"
+            className="bg-gray-50 mb-2 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            onChange={(e) => handleDistrictChange(e)}
+          >
+            <option>Select an option</option>
+            {districtList.map((district: any) => (
+              <option key={district} value={district}>
+                {district}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="max-w-sm mx-auto">
+          <label
+            htmlFor="constituency"
+            className="block mb-2  text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Select Constituency
+          </label>
+          <select
+            id="constituency"
+            onChange={(e) => handleConstituencyChange(e)}
+            className="bg-gray-50 mb-2 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          >
+            <option>Select an option</option>
+            {constituencyList.map((constituency: any) => (
+              <option key={constituency} value={constituency}>
+                {constituency}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="max-w-sm mx-auto">
+          <label
+            htmlFor="assembly"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Select Assembly
+          </label>
+          <select
+            id="assembly"
+            onChange={(e) => handleAssemblyChange(e)}
+            className="bg-gray-50 mb-2 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          >
+            <option>Select an option</option>
+            {assemblyList.map((assembly: any) => (
+              <option key={assembly} value={assembly}>
+                {assembly}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="max-w-sm mx-auto">
+          <label
+            htmlFor="booth"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Select Booth
+          </label>
+          <select
+            id="booth"
+            onChange={(e) => setBooth(e.target.value)}
+            className="bg-gray-50 border mb-2 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          >
+            <option>Select an option</option>
+            {boothList.map((booth: any) => (
+              <option key={booth} value={booth.number}>
+                {booth.number} {booth.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        
+
+        <div className="max-w-sm mx-auto my-5">
+          <button
+            className="bg-primary text-white w-full py-3 rounded-lg bg-blue-500"
+            onClick={handleSubmit}
+          >
+            Add Whatsapp Public
+          </button>
+        </div>
+      </div>
+      <div className="table-list-group my-20">
+                <div className="relative overflow-x-auto">
+                    <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                            <tr>
+                                <th>
+                                    District
+                                </th>
+                                <th>
+                                    Assembly
+                                </th>
+                                <th>
+                                    Constituency
+                                </th>
+                                <th>
+                                    Booth
+                                </th>
+                                <th>
+                                    link
+                                </th>
+                               
+                                <th>
+                                    Optional
+                                </th>
+                                
+                                <th scope="col" className="px-6 py-3">
+                                    Delete
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {whatsappPublic?.map((item: any) => (
+                                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                    <td className="px-6 py-4">{item?.district}</td>
+                                    <td className="px-6 py-4">{item?.assembly}</td>
+                                    <td className="px-6 py-4">{item?.constituency}</td>
+                                    <td className="px-6 py-4">{item?.booth}</td>
+                                    <td className="px-6 py-4">{item?.link}</td>
+                                    <td className="px-6 py-4">{item?.optional}</td>
+                                    <td className="px-6 py-4">
+                                        <button
+                                            className="text-red-700 "
+                                            onClick={() => handleDelete(item._id)}
+                                        >
+                                            Delete
+                                        </button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+    </Sidebar>
+  );
+}
+
+export default AddWhatsappPublic;
