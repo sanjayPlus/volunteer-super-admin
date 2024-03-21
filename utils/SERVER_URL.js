7f4e5fe0636da53c1e2b750b5d7279aa32e00a2a
danaplusitpark <danaplusitpark@gmail.com>
2024-03-21 08:39:42 +0530

    serverurl

--- utils/SERVER_URL.js
+++ utils/SERVER_URL.js
@@ -1,4 +1,5 @@
   // const SERVER_URL = "http://localhost:5000/api";
-  const SERVER_URL = "https://volunteer-4z7g.onrender.com/api";
+  // const SERVER_URL = "https://volunteer-4z7g.onrender.com/api";
+  const SERVER_URL = "https://volunteer-backend.dmckpcc.in/api";
 
 export default SERVER_URL;
