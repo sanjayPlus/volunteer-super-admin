fdd84dac8e5511450ed3b1c7b9060107fc5e2ea5
sanjayPlus <sanjaysanthoshplusitpark@gmail.com>
2024-03-22 15:14:07 +0530

    first commit

--- app/volunteer/[id]/page.tsx
+++ app/volunteer/[id]/page.tsx
@@ -150,12 +150,12 @@ function SingleVolunteer() {
                   Booth Rule
                 </dt>
                 <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                  {user?.boothRule.map((booth:any)=>{return <p>{booth}</p>})}
+                  {user?.boothRule?.map((booth:any)=>{return <p>{booth}</p>})}
                 </dd>
               </div>
               }
                      {
-                user?.aadhaar.length!==0&& <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                user?.aadhaar?.length!==0&& <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                 <dt className="text-sm font-medium text-gray-500">
                 Aadaar Image
                 </dt>
