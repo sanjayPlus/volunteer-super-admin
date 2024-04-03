d5ab5729d8ebfba7f5b8cf4a75d99f2ebe0d57f7
sanjayPlus <sanjaysanthoshplusitpark@gmail.com>
2024-04-03 15:45:53 +0530

    caste added

--- app/update-volunteer/[id]/page.tsx
+++ app/update-volunteer/[id]/page.tsx
@@ -18,6 +18,7 @@ function UpdateVolunteer() {
   const [mandlamPresident, setMandlamPresident] = useState("");
   const [phone, setPhone] = useState("");
   const [districtList, setDistrictList] = useState([]);
+  const [password,setPassword] = useState("")
 
   const [constituencyList, setConstituencyList] = useState([]);
 
@@ -207,6 +208,7 @@ function UpdateVolunteer() {
           mandalamMember,
           mandlamPresident,
           volunteerId: id,
+          password
         },
         {
           headers: {
@@ -274,6 +276,21 @@ function UpdateVolunteer() {
             value={phone}
             className="bg-gray-50 mb-2 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="Enter your phone number"
+          />
+          {/* Password */}
+          <label
+            htmlFor="password"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Password
+          </label>
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            type="text"
+            id="password"
+            value={password}
+            className="bg-gray-50 mb-2 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Enter your Password"
           />
 
           {/* MandalamMember field */}
